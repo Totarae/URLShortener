f2f769e220457371557e8fd2e5c73f04dbda6ae4
grigorii.ivanov <gregoryivanoff6@gmail.com>
2025-02-16 18:30:05 +0300

    Iter8 implement

--- internal/router/router.go
+++ internal/router/router.go
@@ -10,7 +10,10 @@ import (
 // NewRouter создаёт и настраивает маршрутизатор
 func NewRouter(handler *handlers.Handler, logger *zap.Logger) *chi.Mux {
 	r := chi.NewRouter()
-	r.Use(middleware.LoggingMiddleware(logger))
+
+	r.Use(middleware.LoggingMiddleware(logger)) // Подключаем логирование
+	r.Use(middleware.GzipMiddleware)            // Gzip-сжатие
+
 	r.Post("/", handler.ReceiveURL)
 	r.Post("/api/shorten", handler.ReceiveShorten)
 	r.Get("/{id}", handler.ResponseURL)
